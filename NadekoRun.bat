8a6cecb469e936ec12ff1dba20d3916ffcc6e7a9
samvaio <samdivaio@icloud.com>
2016-11-28 20:24:08 +0530

    Update NadekoRun.bat

--- NadekoRun.bat
+++ NadekoRun.bat
@@ -5,5 +5,5 @@ dotnet run --configuration Release
 ECHO NadekoBot has been succesfully stopped, press any key to close this window.
 TITLE NadekoBot - Stopped
 CD /D %~dp0
-timeout /t 5
+PAUSE >nul 2>&1
 del NadekoRunNormal.bat
