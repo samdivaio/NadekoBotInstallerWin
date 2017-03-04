fe0aee13147df49679f97ad7712c4aff493cf4f8
samvaio <samdivaio@icloud.com>
2017-03-05 04:34:37 +0530

    cleaning

--- NadekoInstaller.bat
+++ NadekoInstaller.bat
@@ -31,8 +31,7 @@ ECHO Make sure you are running it on Windows 8 or later.
 timeout /t 10
 TITLE Downloading NadekoBot (Latest), please wait...
 SET "FILENAME=%~dp0\Latest.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/Latest.bat -OutFile %FILENAME%"
-::bitsadmin.exe /transfer "Downloading Nadeko (Latest)" /priority high https://github.com/Kwoth/NadekoBot/raw/master/scripts/Latest.bat "%FILENAME%"
+powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/Latest.bat -OutFile '%FILENAME%'"
 ECHO NadekoBot Dev Build (latest) downloaded.
 timeout /t 5
 CALL Latest.bat
@@ -43,8 +42,7 @@ ECHO Make sure you are running it on Windows 8 or later.
 timeout /t 10
 TITLE Downloading NadekoBot (Stable), please wait...
 SET "FILENAME=%~dp0\Stable.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/Stable.bat -OutFile %FILENAME%"
-::bitsadmin.exe /transfer "Downloading Nadeko (Stable)" /priority high https://github.com/Kwoth/NadekoBot/raw/master/scripts/Stable.bat "%FILENAME%"
+powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/Stable.bat -OutFile '%FILENAME%'"
 ECHO NadekoBot Stable build downloaded.
 timeout /t 5
 CALL Stable.bat
@@ -53,8 +51,7 @@ GOTO End
 :runnormal
 TITLE Downloading NadekoBot Run, please wait...
 SET "FILENAME=%~dp0\NadekoRunNormal.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/NadekoRun.bat -OutFile %FILENAME%"
-::bitsadmin.exe /transfer "Downloading Nadeko Run (normal)" /priority high https://github.com/Kwoth/NadekoBot/raw/master/scripts/NadekoRun.bat "%FILENAME%"
+powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/NadekoRun.bat -OutFile '%FILENAME%'"
 ECHO.
 ECHO Running Nadeko Normally, "if" you are running this to check Nadeko, use ".die" command on discord to stop Nadeko.
 timeout /t 10
@@ -64,8 +61,7 @@ GOTO End
 :autorestart
 TITLE Downloading NadekoBot Auto Run, please wait...
 SET "FILENAME=%~dp0\NadekoAutoRun.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/NadekoAutoRun.bat -OutFile %FILENAME%"
-::bitsadmin.exe /transfer "Downloading Nadeko Auto-Run" /priority high https://github.com/Kwoth/NadekoBot/raw/master/scripts/NadekoAutoRun.bat "%FILENAME%"
+powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/master/scripts/NadekoAutoRun.bat -OutFile '%FILENAME%'"
 ECHO.
 ECHO Running Nadeko with Auto Restart, you will have to close the session to stop the auto restart.
 timeout /t 15
@@ -91,7 +87,7 @@ mkdir %SystemDrive%\ffmpeg\
 SET "FILENAME=%SystemDrive%\ffmpeg\ffmpeg.zip"
 ECHO.
 ECHO Downloading ffmpeg, please wait...
-powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20170225-7e4f32f-win64-static.zip -OutFile %FILENAME%"
+powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20170225-7e4f32f-win64-static.zip -OutFile '%FILENAME%'"
 ECHO.
 ECHO ffmpeg zip downloaded: %FILENAME%...
 ECHO.
@@ -99,7 +95,6 @@ ECHO Press any key to continue extraction...
 pause >nul 2>&1
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170111-e71b811-win64-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170111-e71b811-win64-static"
-::bitsadmin.exe /transfer "Downloading ffmpeg" /priority high https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20170111-e71b811-win64-static.zip "%FILENAME%"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg.zip" powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%SystemDrive%\ffmpeg\ffmpeg.zip"', '%SystemDrive%\ffmpeg\'); }"
 ECHO.
 ECHO ffmpeg extracted to %SystemDrive%\ffmpeg\
@@ -135,15 +130,14 @@ mkdir %SystemDrive%\ffmpeg\
 SET "FILENAME=%SystemDrive%\ffmpeg\ffmpeg.zip"
 ECHO.
 ECHO Downloading ffmpeg, please wait...
-powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20170225-7e4f32f-win32-static.zip -OutFile %FILENAME%"
+powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20170225-7e4f32f-win32-static.zip -OutFile '%FILENAME%'"
 ECHO.
-ECHO ffmpeg zip downloaded: %FILENAME%...
+ECHO ffmpeg zip downloaded: '%FILENAME%'...
 ECHO.
 ECHO Press any key to continue extraction...
 pause >nul 2>&1
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170125-2080bc3-win32-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170125-2080bc3-win32-static"
-::bitsadmin.exe /transfer "Downloading ffmpeg" /priority high https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20170125-2080bc3-win32-static.zip "%FILENAME%"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg.zip" powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%SystemDrive%\ffmpeg\ffmpeg.zip"', '%SystemDrive%\ffmpeg\'); }"
 ECHO.
 ECHO ffmpeg extracted to %SystemDrive%\ffmpeg\
@@ -170,8 +164,7 @@ CALL NadekoInstaller.bat
 :credentials
 TITLE Downloading NadekoBot credentials.json setup files, please wait...
 SET "FILENAME=%~dp0\NadekoCredentials.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBotInstallerWin/raw/master/NadekoCredentials.bat -OutFile %FILENAME%"
-::bitsadmin.exe /transfer "Downloading NadekoBot credentials.json setup files" /priority high https://github.com/samdivaio/NadekoBotInstallerWin/raw/master/NadekoCredentials.bat "%FILENAME%"
+powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBotInstallerWin/raw/master/NadekoCredentials.bat -OutFile '%FILENAME%'"
 ECHO NadekoBot credentials.json setup files downloaded.
 timeout /t 5
 CALL NadekoCredentials.bat
