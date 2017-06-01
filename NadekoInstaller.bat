ce29c9ee304cb5ea38be49e2f3e0fcdaa7988764
samvaio <samdivaio@icloud.com>
2017-06-01 16:39:18 +0530

    all set ffmpeg updated too

--- NadekoInstaller.bat
+++ NadekoInstaller.bat
@@ -2,6 +2,29 @@
 TITLE NadekoBot Installer!
 SET "root=%~dp0"
 CD /D "%root%"
+goto check_Permissions
+timeout /t 5
+goto installer
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+	echo.
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+		echo.
+		pause
+    ) else (
+        echo Failure: Current permissions inadequate.
+		echo.
+		echo Run again as Administrator.
+		echo.
+		pause >nul
+		goto exit
+    )
+
+:installer
 IF EXIST "%root%windowsAIO.bat" del "%root%windowsAIO.bat"
 IF EXIST "%root%NadekoCredentials.bat" del "%root%NadekoCredentials.bat"
 IF EXIST "%root%NadekoAutoRun.bat" del "%root%NadekoAutoRun.bat"
@@ -9,7 +32,7 @@ IF EXIST "%root%NadekoRun.bat" del "%root%NadekoRun.bat"
 IF EXIST "%root%Stable.bat" del "%root%Stable.bat"
 IF EXIST "%root%Latest.bat" del "%root%Latest.bat"
 SET "FILENAME=%root%windowsAIO.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBotInstallerWin/raw/master/windowsAIO.bat -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest https://github.com/samdivaio/NadekoBotInstallerWin/raw/1.4/windowsAIO.bat -OutFile '%FILENAME%'"
 CALL windowsAIO.bat
 IF EXIST "%root%windowsAIO.bat" del "%root%windowsAIO.bat"
 IF EXIST "%root%NadekoCredentials.bat" del "%root%NadekoCredentials.bat"
@@ -18,3 +41,7 @@ IF EXIST "%root%NadekoRun.bat" del "%root%NadekoRun.bat"
 IF EXIST "%root%Stable.bat" del "%root%Stable.bat"
 IF EXIST "%root%Latest.bat" del "%root%Latest.bat"
 pause
+
+
+:exit
+exit
--- windowsAIO.bat
+++ windowsAIO.bat
@@ -31,7 +31,7 @@ ECHO Make sure you are running it on Windows 8 or later.
 timeout /t 10
 TITLE Downloading NadekoBot (Latest), please wait...
 SET "FILENAME=%~dp0\Latest.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/dev/scripts/Latest.bat -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest https://github.com/samdivaio/NadekoBotInstallerWin/raw/1.4/Latest.bat -OutFile '%FILENAME%'"
 ECHO NadekoBot Dev Build (latest) downloaded.
 timeout /t 5
 CALL Latest.bat
@@ -42,7 +42,7 @@ ECHO Make sure you are running it on Windows 8 or later.
 timeout /t 10
 TITLE Downloading NadekoBot (Stable), please wait...
 SET "FILENAME=%~dp0\Stable.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/dev/scripts/Stable.bat -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest https://github.com/samdivaio/NadekoBotInstallerWin/raw/1.4/Stable.bat -OutFile '%FILENAME%'"
 ECHO NadekoBot Stable build downloaded.
 timeout /t 5
 CALL Stable.bat
@@ -51,7 +51,7 @@ GOTO End
 :runnormal
 TITLE Downloading NadekoBot Run, please wait...
 SET "FILENAME=%~dp0\NadekoRunNormal.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/dev/scripts/NadekoRun.bat -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest https://github.com/samdivaio/NadekoBotInstallerWin/raw/1.4/NadekoRun.bat -OutFile '%FILENAME%'"
 ECHO.
 ECHO Running Nadeko Normally, "if" you are running this to check Nadeko, use ".die" command on discord to stop Nadeko.
 timeout /t 10
@@ -61,7 +61,7 @@ GOTO End
 :autorestart
 TITLE Downloading NadekoBot Auto Run, please wait...
 SET "FILENAME=%~dp0\NadekoAutoRun.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBot/raw/dev/scripts/NadekoAutoRun.bat -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest https://github.com/samdivaio/NadekoBotInstallerWin/raw/1.4/NadekoAutoRun.bat -OutFile '%FILENAME%'"
 ECHO.
 ECHO Running Nadeko with Auto Restart, you will have to close the session to stop the auto restart.
 timeout /t 15
@@ -107,7 +107,7 @@ mkdir "%SystemDrive%\ffmpeg\"
 SET "FILENAME=%SystemDrive%\ffmpeg\ffmpeg.zip"
 ECHO.
 ECHO Downloading ffmpeg, please wait...
-powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20170225-7e4f32f-win64-static.zip -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20170601-bd1179e-win64-static.zip -OutFile '%FILENAME%'"
 ECHO.
 ECHO ffmpeg zip downloaded: %FILENAME%...
 ECHO.
@@ -115,6 +115,7 @@ ECHO Press any key to continue extraction...
 pause >nul 2>&1
 ECHO.
 ECHO Extracting files...
+IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170601-bd1179e-win64-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170601-bd1179e-win64-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170111-e71b811-win64-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170111-e71b811-win64-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg.zip" powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%SystemDrive%\ffmpeg\ffmpeg.zip"', '%SystemDrive%\ffmpeg\'); }"
@@ -131,7 +132,7 @@ pause
 reg export "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" "%SystemDrive%\ffmpeg\path_registry_backup.reg"
 ECHO Registry file backup complete!
 @echo on
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v "path" /t REG_SZ /d "%path%;%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static\bin"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v "path" /t REG_SZ /d "%path%;%SystemDrive%\ffmpeg\ffmpeg-20170601-bd1179e-win64-static\bin"
 @echo off
 ECHO ffmpeg path has been set!
 ECHO.
@@ -157,7 +158,7 @@ mkdir "%SystemDrive%\ffmpeg\"
 SET "FILENAME=%SystemDrive%\ffmpeg\ffmpeg.zip"
 ECHO.
 ECHO Downloading ffmpeg, please wait...
-powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20170225-7e4f32f-win32-static.zip -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20170601-bd1179e-win32-static.zip -OutFile '%FILENAME%'"
 ECHO.
 ECHO ffmpeg zip downloaded: '%FILENAME%'...
 ECHO.
@@ -165,6 +166,7 @@ ECHO Press any key to continue extraction...
 pause >nul 2>&1
 ECHO.
 ECHO Extracting files...
+IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170601-bd1179e-win32-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170601-bd1179e-win32-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170125-2080bc3-win32-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170125-2080bc3-win32-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg.zip" powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%SystemDrive%\ffmpeg\ffmpeg.zip"', '%SystemDrive%\ffmpeg\'); }"
@@ -181,7 +183,7 @@ pause
 reg export "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" "%SystemDrive%\ffmpeg\path_registry_backup.reg"
 ECHO Registry file backup complete!
 @echo on
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v "path" /t REG_SZ /d "%path%;%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static\bin"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v "path" /t REG_SZ /d "%path%;%SystemDrive%\ffmpeg\ffmpeg-20170601-bd1179e-win32-static\bin"
 @echo off
 ECHO ffmpeg path has been set!
 ECHO.
@@ -197,7 +199,7 @@ CALL NadekoInstaller.bat
 :credentials
 TITLE Downloading NadekoBot credentials.json setup files, please wait...
 SET "FILENAME=%~dp0\NadekoCredentials.bat"
-powershell -Command "Invoke-WebRequest https://github.com/Kwoth/NadekoBotInstallerWin/raw/master/NadekoCredentials.bat -OutFile '%FILENAME%'"
+powershell -Command "Invoke-WebRequest https://github.com/samdivaio/NadekoBotInstallerWin/raw/1.4/NadekoCredentials.bat -OutFile '%FILENAME%'"
 ECHO NadekoBot credentials.json setup files downloaded.
 timeout /t 5
 CALL NadekoCredentials.bat
